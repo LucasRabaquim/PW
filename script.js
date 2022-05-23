38bd7b13c3e90eddd253adfb9e848c996b3397f5
Lucas Daniel da Costa Rabaquim <98958822+LucasRabaquim@users.noreply.github.com>
2022-05-23 11:59:27 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,35 @@
-let $tituloPagina = document.querySelector("#titulo");
+const $tituloPagina = document.querySelector("#titulo");
 // 1. Retorna apenas o primeiro elemento que corresponde aos seletores especificados.
 $tituloPagina.textContent = "ETEC Professor Basilides de Godoy";
 // 3. A propriedade textContent define ou retorna o conteúdo de texto.
-let $tabela = document.querySelector("#tabela-aluno");
-// console.log($tabela);
-let $registroAluno = document.querySelectorAll(".aluno");
-console.log($registroAluno);
+const $registroAluno = document.querySelectorAll(".aluno");
 
-for(let n = 0; n < $registroAluno.length; n++){
-    let $registro = $registroAluno[n];
-    //let $nome = $contagem.querySelector(".td-nome").textContent;
-    let $n1 = nota(".td-n1",$registro);
-    let $n2 = nota(".td-n2",$registro);
-    let $n3 = nota(".td-n3",$registro);
-    //let $mediaFinal = calcularMedia($n1, $n2, $n3);
-    let $mediaFinal = ((a+b+c) / 3).toFixed(2);
-    let $media = $registro.querySelector(".td-media");
+$registroAluno.forEach($registro => {
+    const $nota = $registro.querySelectorAll('.td-n');
+    let $result = 0;
+    $nota.forEach(nota =>  $result += parseFloat(nota.textContent)  );    
+
+    const $mediaFinal = ($result / $nota.length).toFixed(2);
+    const $media = $registro.querySelector(".td-media");
     $media.textContent = $mediaFinal;
     $media.style.color = ($mediaFinal >= 6) ? "blue" : "red";
     $media.style.fontWeight = "bold";
-    console.log($media)
-}
-function nota(nota,registro){
-    return parseFloat(registro.querySelector(nota).textContent);
+    const $situacao = $registro.querySelector('.td-situacao');
+    $situacao.textContent = mostrarSituacao($mediaFinal,0);
+    $situacao.classList.add(mostrarSituacao($mediaFinal,1));
+})
+function mostrarSituacao(x,index){
+    const $resultado = ["Situação","estilo"];
+    $resultado[0] = (x < 6) ? "Reprovado" : "Aprovado";
+    $resultado[1] = (x < 6) ? "reprovado" : "aprovado";
+    return $resultado[index];
 }
 /*
-function calcularMedia(a,b,c){
-    return ((a+b+c) / 3).toFixed(2);
-}
+const dasda = x => ({
+    estado: x < 6 ? "Reprovado" : "Aprovado",
+    estilo: x < 6 ? "reprovado" : "aprovado"
+})
+// const um = [0, 1, 2, 3].reduce((accumulator, currentValue) =>  accumulator + currentValue, 0);
+//   // sum is 6
+//   console.log(sum);
 */
